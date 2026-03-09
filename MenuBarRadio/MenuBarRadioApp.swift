cf780ccb398beadb818e1aa81031e0db6b12106b
Dirk Clemens <git@adcore.de>
2026-03-09 19:18:11 +0100

    Initial Commit

---
+++ MenuBarRadio/MenuBarRadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarRadioApp.swift
+//  MenuBarRadio
+//
+//  Created by Dirk Clemens on 09.03.26.
+//
+
+import SwiftUI
+
+@main
+struct MenuBarRadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
