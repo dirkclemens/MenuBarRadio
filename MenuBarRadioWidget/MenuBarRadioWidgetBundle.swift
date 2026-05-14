c9f61c901f7718238b715f5e2ec84bba17527944
Dirk Clemens <git@adcore.de>
2026-05-14 18:10:55 +0200

    screenshot updated

---
+++ MenuBarRadioWidget/MenuBarRadioWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarRadioWidgetBundle.swift
+//  MenuBarRadioWidget
+//
+//  Created by Dirk Clemens on 05.04.26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MenuBarRadioWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MenuBarRadioWidget()
+        MenuBarRadioWidgetControl()
+    }
+}
